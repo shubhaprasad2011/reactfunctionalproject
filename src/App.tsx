c58e510af3607dde80d926504def0726db54c748
shubhaprasad2011 <shubhaprasad2011@gmail.com>
2025-04-10 10:15:35 +0530

    updated from function to FC (functional component)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css';
 import Header from '../src/Components/Header'
-import React from 'react';
+import React, { JSX } from 'react';
 
-function App() {
+const App: React.FC = (): JSX.Element => {
+
   return (
     <div className="App">
       <Header></Header>
     </div>
   );
-}
+};
 
 export default App;
