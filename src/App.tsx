531a4078207485f906f05f06fb42c96496f70a9e
shubhaprasad2011 <shubhaprasad2011@gmail.com>
2025-04-10 09:16:58 +0530

    convert js to tsx application

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from '../src/Components/Header'
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
